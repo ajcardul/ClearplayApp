5c441f32edcf6bfca793e15a9d4e91cd96543fd5
ajcardul <ajcardul@ncsu.edu>
2024-01-18 13:24:53 -0500

    Failed attempt at sound and pieceChecking

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -13,47 +13,47 @@ import androidx.activity.ComponentActivity
 
 class MainActivity : ComponentActivity() {
     private var squareView: View? = null
-
-
+    val PieceTracker: BooleanArray = BooleanArray(2)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val myButton = findViewById<Button>(R.id.button)
-        val myButton2 = findViewById<Button>(R.id.button2)
-        val myButton3 = findViewById<Button>(R.id.button3)
-        val myButton4 = findViewById<Button>(R.id.button4)
-        val myButton5 = findViewById<Button>(R.id.button5)
+        val startButton = findViewById<Button>(R.id.startButton)
+        val optionsButton = findViewById<Button>(R.id.optionsButton)
+        val topButton = findViewById<Button>(R.id.topButton)
+        val botButton = findViewById<Button>(R.id.botButton)
+        val baseButton = findViewById<Button>(R.id.baseButton)
         val resetButton = findViewById<Button>(R.id.reset)
         val mainMenuButton = findViewById<Button>(R.id.mainMenu)
         val slideShowButton = findViewById<Button>(R.id.slideShowButton)
         val importButton = findViewById<Button>(R.id.importButton)
         val imageView = findViewById<ImageView>(R.id.imageView)
-        var mediaPlayer1 = MediaPlayer.create(this, R.raw.pieceone)
-        var mediaPlayer2 = MediaPlayer.create(this, R.raw.piecetwo)
+        var mediaPlayer1 = MediaPlayer.create(this , R.raw.pieceone)
+        var mediaPlayer2 = MediaPlayer.create(this , R.raw.piecetwo)
 
-
-        myButton3.visibility = View.INVISIBLE
-        myButton3.setBackgroundColor(Color.TRANSPARENT)
-        myButton3.setTextColor(Color.TRANSPARENT)
-        myButton4.visibility = View.INVISIBLE
-        myButton4.setBackgroundColor(Color.TRANSPARENT)
-        myButton4.setTextColor(Color.TRANSPARENT)
+        //initial setting
+        topButton.visibility = View.INVISIBLE
+        topButton.setBackgroundColor(Color.TRANSPARENT)
+        topButton.setTextColor(Color.TRANSPARENT)
+        botButton.visibility = View.INVISIBLE
+        botButton.setBackgroundColor(Color.TRANSPARENT)
+        botButton.setTextColor(Color.TRANSPARENT)
         resetButton.visibility = View.INVISIBLE
         mainMenuButton.visibility = View.INVISIBLE
-        myButton5.visibility = View.VISIBLE
-        myButton5.setBackgroundColor(Color.TRANSPARENT)
-        myButton5.setTextColor(Color.TRANSPARENT)
+        baseButton.visibility = View.VISIBLE
+        baseButton.setBackgroundColor(Color.TRANSPARENT)
+        baseButton.setTextColor(Color.TRANSPARENT)
 
 
-        myButton.setOnClickListener {
-            myButton.visibility = View.INVISIBLE
-            myButton5.visibility = View.INVISIBLE
-            myButton2.visibility = View.INVISIBLE
+        startButton.setOnClickListener {
+            print("START")
+            startButton.visibility = View.INVISIBLE
+            baseButton.visibility = View.INVISIBLE
+            optionsButton.visibility = View.INVISIBLE
             slideShowButton.visibility = View.INVISIBLE
             importButton.visibility = View.INVISIBLE
             imageView.visibility = View.INVISIBLE
-            myButton3.visibility = View.VISIBLE
-            myButton4.visibility = View.VISIBLE
+            topButton.visibility = View.VISIBLE
+            botButton.visibility = View.VISIBLE
             resetButton.visibility = View.VISIBLE
             mainMenuButton.visibility = View.VISIBLE
 
@@ -61,71 +61,52 @@ class MainActivity : ComponentActivity() {
 
         }
         mainMenuButton.setOnClickListener {
-            myButton.visibility = View.VISIBLE
-            myButton5.visibility = View.VISIBLE
-            myButton2.visibility = View.VISIBLE
+            startButton.visibility = View.VISIBLE
+            baseButton.visibility = View.VISIBLE
+            optionsButton.visibility = View.VISIBLE
             slideShowButton.visibility = View.VISIBLE
             importButton.visibility = View.VISIBLE
             imageView.visibility = View.VISIBLE
-            myButton3.visibility = View.INVISIBLE
-            myButton4.visibility = View.INVISIBLE
+            topButton.visibility = View.INVISIBLE
+            botButton.visibility = View.INVISIBLE
             resetButton.visibility = View.INVISIBLE
             mainMenuButton.visibility = View.INVISIBLE
 
             findViewById<RelativeLayout>(R.id.container).setBackgroundColor(0xFF085747.toInt())
 
         }
-        myButton3.setOnClickListener {
-            mediaPlayer1.start()
-            myButton3.setBackgroundColor(Color.GREEN)
-
+        topButton.setOnClickListener {
+            //mediaPlayer1.start()
+            topButton.setBackgroundColor(Color.GREEN)
+            PieceTracker[0] = true
+            if (checkCompletion()) {
+                print("hi")
+                mediaPlayer1.start()
+            }
         }
-        myButton4.setOnClickListener {
-            mediaPlayer2.start()
-            myButton4.setBackgroundColor(Color.YELLOW)
-
+        botButton.setOnClickListener {
+            //mediaPlayer2.start()
+            botButton.setBackgroundColor(Color.YELLOW)
+            PieceTracker[1] = true
+            if (checkCompletion()) {
+                mediaPlayer2.start()
+            }
         }
         resetButton.setOnClickListener {
-            myButton3.setBackgroundColor(Color.TRANSPARENT)
-            myButton4.setBackgroundColor(Color.TRANSPARENT)
-
+            topButton.setBackgroundColor(Color.TRANSPARENT)
+            botButton.setBackgroundColor(Color.TRANSPARENT)
+            for(i in 0 until 2) {
+                PieceTracker[i] = false
+            }
         }
     }
-
-    override fun onTouchEvent(event: MotionEvent): Boolean {
-        val container = findViewById<RelativeLayout>(R.id.container)
-        val x = event.x
-        val y = event.y
-        var mediaPlayer1 = MediaPlayer.create(this, R.raw.goomba)
-
-        when (event.action) {
-            MotionEvent.ACTION_DOWN -> {
-                // Create a green square when touch is pressed
-                squareView = View(this)
-                squareView?.setBackgroundColor(Color.GREEN)
-                val squareSize = 100 // Adjust the square size as needed
-                val params = ViewGroup.LayoutParams(squareSize, squareSize)
-                squareView?.layoutParams = params
-                squareView?.x = x - squareSize / 2
-                squareView?.y = y - squareSize / 2
-                container.addView(squareView)
-                //mediaPlayer1.start()
-            }
-            MotionEvent.ACTION_MOVE -> {
-                // Move the green square with the cursor during touch
-                if (squareView != null) {
-                    squareView?.x = x - squareView?.width!! / 2
-                    squareView?.y = y - squareView?.height!! / 2
-                }
-            }
-            MotionEvent.ACTION_UP -> {
-                // Remove the green square when touch is released
-                if (squareView != null) {
-                    container.removeView(squareView)
-                }
+    fun checkCompletion(): Boolean {
+        print("1")
+        for(i in 0 until 2) {
+            if (!PieceTracker[i]) {
+                return false
             }
         }
-
         return true
     }
 }

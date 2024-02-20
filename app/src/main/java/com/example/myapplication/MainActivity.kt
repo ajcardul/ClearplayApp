a9421c8930b35f862560f97b3e0d5117de1110d1
KPichon02 <kylepichon@gmail.com>
2024-02-19 23:05:13 -0500

    Changes for reading in text files to control subpuzzles. Additional global variables created for mediaplayers

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,129 +3,345 @@ package com.example.myapplication
 import android.graphics.Color
 import android.media.MediaPlayer
 import android.os.Bundle
-import android.view.MotionEvent
 import android.view.View
-import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.RelativeLayout
 import androidx.activity.ComponentActivity
+import java.util.Stack
+import java.io.File
 
 class MainActivity : ComponentActivity() {
     private var squareView: View? = null
+    val PieceTracker: BooleanArray = BooleanArray(12)
+    var stack: Stack<Int> = Stack()
+    var SPData:  IntArray = IntArray(12)
+    //var SPData: IntArray = intArrayOf(0, 1, 1, 2, 0, 1, 1, 2, 0, 0, 2, 2)
+    val mediaPlayer1: MediaPlayer by lazy {MediaPlayer.create(this , R.raw.pieceone)}
+    val mediaPlayer2: MediaPlayer by lazy {MediaPlayer.create(this , R.raw.piecetwo)}
 
+    var pNum: Int = 0
+    var numPieces: Int = 0
+    var numSPs: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val myButton = findViewById<Button>(R.id.button)
-        val myButton2 = findViewById<Button>(R.id.button2)
-        val myButton3 = findViewById<Button>(R.id.button3)
-        val myButton4 = findViewById<Button>(R.id.button4)
-        val myButton5 = findViewById<Button>(R.id.button5)
+        val startButton = findViewById<Button>(R.id.startButton)
+        val optionsButton = findViewById<Button>(R.id.optionsButton)
+        val button1 = findViewById<Button>(R.id.button1)
+        val button2 = findViewById<Button>(R.id.button2)
+        val button3 = findViewById<Button>(R.id.button3)
+        val button4 = findViewById<Button>(R.id.button4)
+        val button5 = findViewById<Button>(R.id.button5)
+        val button6 = findViewById<Button>(R.id.button6)
+        val button7 = findViewById<Button>(R.id.button7)
+        val button8 = findViewById<Button>(R.id.button8)
+        val button9 = findViewById<Button>(R.id.button9)
+        val button10 = findViewById<Button>(R.id.button10)
+        val button11 = findViewById<Button>(R.id.button11)
+        val button12 = findViewById<Button>(R.id.button12)
+        val baseButton = findViewById<Button>(R.id.baseButton)
         val resetButton = findViewById<Button>(R.id.reset)
+        val temp = findViewById<Button>(R.id.temp)
+        val temp2 = findViewById<Button>(R.id.temp2)
         val mainMenuButton = findViewById<Button>(R.id.mainMenu)
         val slideShowButton = findViewById<Button>(R.id.slideShowButton)
         val importButton = findViewById<Button>(R.id.importButton)
         val imageView = findViewById<ImageView>(R.id.imageView)
-        var mediaPlayer1 = MediaPlayer.create(this, R.raw.pieceone)
-        var mediaPlayer2 = MediaPlayer.create(this, R.raw.piecetwo)
-
-
-        myButton3.visibility = View.INVISIBLE
-        myButton3.setBackgroundColor(Color.TRANSPARENT)
-        myButton3.setTextColor(Color.TRANSPARENT)
-        myButton4.visibility = View.INVISIBLE
-        myButton4.setBackgroundColor(Color.TRANSPARENT)
-        myButton4.setTextColor(Color.TRANSPARENT)
+        //var mediaPlayer1 = MediaPlayer.create(this , R.raw.pieceone)
+        //var mediaPlayer2 = MediaPlayer.create(this , R.raw.piecetwo)
+        window.decorView.apply{
+            systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
+                    or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_FULLSCREEN
+                    or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    )
+        }
+        //initial setting
+        button1.visibility = View.INVISIBLE
+        button1.setBackgroundColor(Color.TRANSPARENT)
+        button1.setTextColor(Color.TRANSPARENT)
+        button2.visibility = View.INVISIBLE
+        button2.setBackgroundColor(Color.TRANSPARENT)
+        button2.setTextColor(Color.TRANSPARENT)
+        button3.visibility = View.INVISIBLE
+        button3.setBackgroundColor(Color.TRANSPARENT)
+        button3.setTextColor(Color.TRANSPARENT)
+        button4.visibility = View.INVISIBLE
+        button4.setBackgroundColor(Color.TRANSPARENT)
+        button4.setTextColor(Color.TRANSPARENT)
+        button5.visibility = View.INVISIBLE
+        button5.setBackgroundColor(Color.TRANSPARENT)
+        button5.setTextColor(Color.TRANSPARENT)
+        button6.visibility = View.INVISIBLE
+        button6.setBackgroundColor(Color.TRANSPARENT)
+        button6.setTextColor(Color.TRANSPARENT)
+        button7.visibility = View.INVISIBLE
+        button7.setBackgroundColor(Color.TRANSPARENT)
+        button7.setTextColor(Color.TRANSPARENT)
+        button8.visibility = View.INVISIBLE
+        button8.setBackgroundColor(Color.TRANSPARENT)
+        button8.setTextColor(Color.TRANSPARENT)
+        button9.visibility = View.INVISIBLE
+        button9.setBackgroundColor(Color.TRANSPARENT)
+        button9.setTextColor(Color.TRANSPARENT)
+        button10.visibility = View.INVISIBLE
+        button10.setBackgroundColor(Color.TRANSPARENT)
+        button10.setTextColor(Color.TRANSPARENT)
+        button11.visibility = View.INVISIBLE
+        button11.setBackgroundColor(Color.TRANSPARENT)
+        button11.setTextColor(Color.TRANSPARENT)
+        button12.visibility = View.INVISIBLE
+        button12.setBackgroundColor(Color.TRANSPARENT)
+        button12.setTextColor(Color.TRANSPARENT)
         resetButton.visibility = View.INVISIBLE
         mainMenuButton.visibility = View.INVISIBLE
-        myButton5.visibility = View.VISIBLE
-        myButton5.setBackgroundColor(Color.TRANSPARENT)
-        myButton5.setTextColor(Color.TRANSPARENT)
+        temp.visibility = View.INVISIBLE
+        temp2.visibility = View.INVISIBLE
+
+        baseButton.visibility = View.VISIBLE
+        baseButton.setBackgroundColor(Color.TRANSPARENT)
+        baseButton.setTextColor(Color.TRANSPARENT)
 
 
-        myButton.setOnClickListener {
-            myButton.visibility = View.INVISIBLE
-            myButton5.visibility = View.INVISIBLE
-            myButton2.visibility = View.INVISIBLE
+        startButton.setOnClickListener {
+            startButton.visibility = View.INVISIBLE
+            baseButton.visibility = View.INVISIBLE
+            optionsButton.visibility = View.INVISIBLE
             slideShowButton.visibility = View.INVISIBLE
             importButton.visibility = View.INVISIBLE
             imageView.visibility = View.INVISIBLE
-            myButton3.visibility = View.VISIBLE
-            myButton4.visibility = View.VISIBLE
+            button1.visibility = View.VISIBLE
+            button2.visibility = View.VISIBLE
+            button3.visibility = View.VISIBLE
+            button4.visibility = View.VISIBLE
+            button5.visibility = View.VISIBLE
+            button6.visibility = View.VISIBLE
+            button7.visibility = View.VISIBLE
+            button8.visibility = View.VISIBLE
+            button9.visibility = View.VISIBLE
+            button10.visibility = View.VISIBLE
+            button11.visibility = View.VISIBLE
+            button12.visibility = View.VISIBLE
             resetButton.visibility = View.VISIBLE
             mainMenuButton.visibility = View.VISIBLE
+            temp.visibility = View.VISIBLE
+            temp2.visibility = View.VISIBLE
 
             findViewById<RelativeLayout>(R.id.container).setBackgroundColor(0xFFFFFFFF.toInt())
 
         }
         mainMenuButton.setOnClickListener {
-            myButton.visibility = View.VISIBLE
-            myButton5.visibility = View.VISIBLE
-            myButton2.visibility = View.VISIBLE
+            startButton.visibility = View.VISIBLE
+            baseButton.visibility = View.VISIBLE
+            optionsButton.visibility = View.VISIBLE
             slideShowButton.visibility = View.VISIBLE
             importButton.visibility = View.VISIBLE
             imageView.visibility = View.VISIBLE
-            myButton3.visibility = View.INVISIBLE
-            myButton4.visibility = View.INVISIBLE
+            button1.visibility = View.INVISIBLE
+            button2.visibility = View.INVISIBLE
+            button3.visibility = View.INVISIBLE
+            button4.visibility = View.INVISIBLE
+            button5.visibility = View.INVISIBLE
+            button6.visibility = View.INVISIBLE
+            button7.visibility = View.INVISIBLE
+            button8.visibility = View.INVISIBLE
+            button9.visibility = View.INVISIBLE
+            button10.visibility = View.INVISIBLE
+            button11.visibility = View.INVISIBLE
+            button12.visibility = View.INVISIBLE
             resetButton.visibility = View.INVISIBLE
             mainMenuButton.visibility = View.INVISIBLE
-
+            temp.visibility = View.INVISIBLE
+            temp2.visibility = View.INVISIBLE
             findViewById<RelativeLayout>(R.id.container).setBackgroundColor(0xFF085747.toInt())
 
         }
-        myButton3.setOnClickListener {
-            mediaPlayer1.start()
-            myButton3.setBackgroundColor(Color.GREEN)
-
+        button1.setOnClickListener {
+            //topButton.setBackgroundColor(Color.GREEN)
+            placePiece(0)
+            SPChecker(0)
         }
-        myButton4.setOnClickListener {
-            mediaPlayer2.start()
-            myButton4.setBackgroundColor(Color.YELLOW)
+        button2.setOnClickListener {
+            //botButton.setBackgroundColor(Color.YELLOW)
+            placePiece(1)
+            SPChecker(1)
+        }
+        button3.setOnClickListener {
+            placePiece(2)
+            SPChecker(2)
+        }
 
+        button4.setOnClickListener {
+            placePiece(3)
+            SPChecker(3)
+        }
+
+        button5.setOnClickListener {
+            placePiece(4)
+            SPChecker(4)
+        }
+
+        button6.setOnClickListener {
+            placePiece(5)
+            SPChecker(5)
+        }
+
+        button7.setOnClickListener {
+            placePiece(6)
+            SPChecker(6)
+        }
+
+        button8.setOnClickListener {
+            placePiece(7)
+            SPChecker(7)
+        }
+
+        button9.setOnClickListener {
+            placePiece(8)
+            SPChecker(8)
+        }
+
+        button10.setOnClickListener {
+            placePiece(9)
+            SPChecker(9)
+        }
+
+        button11.setOnClickListener {
+            placePiece(10)
+            SPChecker(10)
+        }
+        button12.setOnClickListener {
+            placePiece(11)
+            SPChecker(11)
         }
         resetButton.setOnClickListener {
-            myButton3.setBackgroundColor(Color.TRANSPARENT)
-            myButton4.setBackgroundColor(Color.TRANSPARENT)
-
+            if(!stack.isEmpty()){
+                var poppedPiece = stack.pop()
+                removePiece(poppedPiece);
+            }
+            /* ORIGINAL RESET CODE
+            topButton.setBackgroundColor(Color.TRANSPARENT)
+            botButton.setBackgroundColor(Color.TRANSPARENT)
+            for(i in 0 until 2) {
+                PieceTracker[i] = false
+            }
+            */
+        }
+        importButton.setOnClickListener {
+            readInfo()
+            pNum = (pNum + 1) % 2
         }
     }
-
-    override fun onTouchEvent(event: MotionEvent): Boolean {
-        val container = findViewById<RelativeLayout>(R.id.container)
-        val x = event.x
-        val y = event.y
-        var mediaPlayer1 = MediaPlayer.create(this, R.raw.goomba)
-
-        when (event.action) {
-            MotionEvent.ACTION_DOWN -> {
-                // Create a green square when touch is pressed
-                squareView = View(this)
-                squareView?.setBackgroundColor(Color.GREEN)
-                val squareSize = 100 // Adjust the square size as needed
-                val params = ViewGroup.LayoutParams(squareSize, squareSize)
-                squareView?.layoutParams = params
-                squareView?.x = x - squareSize / 2
-                squareView?.y = y - squareSize / 2
-                container.addView(squareView)
-                //mediaPlayer1.start()
+    fun checkCompletion(): Boolean {
+        for(i in 0 until 12) {
+            if (!PieceTracker[i]) {
+                return false
             }
-            MotionEvent.ACTION_MOVE -> {
-                // Move the green square with the cursor during touch
-                if (squareView != null) {
-                    squareView?.x = x - squareView?.width!! / 2
-                    squareView?.y = y - squareView?.height!! / 2
+        }
+        return true
+    }
+
+    fun SPChecker(num: Int): Boolean {
+        var SPNum: Int = SPData[num]
+        for(i in 0 until 12) {
+            if (SPData[i] == SPNum) {
+                if (!PieceTracker[i]) {
+                    return false
                 }
             }
-            MotionEvent.ACTION_UP -> {
-                // Remove the green square when touch is released
-                if (squareView != null) {
-                    container.removeView(squareView)
-                }
-            }
+        }
+        if (SPNum == 1) {
+            mediaPlayer1.start()
+        }
+        if (SPNum == 2) {
+            mediaPlayer2.start()
+        }
+        return true
+    }
+    fun placePiece(x: Int): Boolean {
+        if(PieceTracker[x])
+            return true
+        PieceTracker[x] = true
+        var color: Int = 0xFFFFFFFF.toInt()
+        if (SPData[x] == 0) {
+            color = 0xFFFF0000.toInt()
+        }
+        else if (SPData[x] == 1) {
+            color = 0xFF00FF00.toInt()
+        }
+        else if (SPData[x] == 2) {
+            color = 0xFF0000FF.toInt()
+        }
+        when (x) {
+            0 -> findViewById<Button>(R.id.button1).setBackgroundColor(color)
+            1 -> findViewById<Button>(R.id.button2).setBackgroundColor(color)
+            2 -> findViewById<Button>(R.id.button3).setBackgroundColor(color)
+            3 -> findViewById<Button>(R.id.button4).setBackgroundColor(color)
+            4 -> findViewById<Button>(R.id.button5).setBackgroundColor(color)
+            5 -> findViewById<Button>(R.id.button6).setBackgroundColor(color)
+            6 -> findViewById<Button>(R.id.button7).setBackgroundColor(color)
+            7 -> findViewById<Button>(R.id.button8).setBackgroundColor(color)
+            8 -> findViewById<Button>(R.id.button9).setBackgroundColor(color)
+            9 -> findViewById<Button>(R.id.button10).setBackgroundColor(color)
+            10 -> findViewById<Button>(R.id.button11).setBackgroundColor(color)
+            11 -> findViewById<Button>(R.id.button12).setBackgroundColor(color)
+        }
+        stack.push(x)
+        return true
+    }
+    fun removePiece(x: Int): Boolean {
+        PieceTracker[x] = false
+
+        when (x) {
+            0 -> findViewById<Button>(R.id.button1).setBackgroundColor(Color.TRANSPARENT)
+            1 -> findViewById<Button>(R.id.button2).setBackgroundColor(Color.TRANSPARENT)
+            2 -> findViewById<Button>(R.id.button3).setBackgroundColor(Color.TRANSPARENT)
+            3 -> findViewById<Button>(R.id.button4).setBackgroundColor(Color.TRANSPARENT)
+            4 -> findViewById<Button>(R.id.button5).setBackgroundColor(Color.TRANSPARENT)
+            5 -> findViewById<Button>(R.id.button6).setBackgroundColor(Color.TRANSPARENT)
+            6 -> findViewById<Button>(R.id.button7).setBackgroundColor(Color.TRANSPARENT)
+            7 -> findViewById<Button>(R.id.button8).setBackgroundColor(Color.TRANSPARENT)
+            8 -> findViewById<Button>(R.id.button9).setBackgroundColor(Color.TRANSPARENT)
+            9 -> findViewById<Button>(R.id.button10).setBackgroundColor(Color.TRANSPARENT)
+            10 -> findViewById<Button>(R.id.button11).setBackgroundColor(Color.TRANSPARENT)
+            11 -> findViewById<Button>(R.id.button12).setBackgroundColor(Color.TRANSPARENT)
         }
 
         return true
+    }
+
+    fun readInfo() {
+        // Specify the path to your text file
+        var filePath: String = ""
+        if (pNum == 0) {
+            filePath = "p0data.txt"
+        }
+        else {
+            filePath = "p1data.txt"
+        }
+
+        val lines = applicationContext.assets.open(filePath).bufferedReader().use {
+            it.readLines()
+        }
+
+        if (lines.size >= 3) {
+            // Parse the values from the lines
+            numPieces = lines[0].toIntOrNull() ?: 0
+            numSPs = lines[1].toIntOrNull() ?: 0
+            // Leave as string, convert to charArray, then do individual char to number and fill in SPData
+            val tempNum = lines[2]
+            val numChars = tempNum.toCharArray()
+            for (i in numChars.indices) {
+                SPData[i] = Character.getNumericValue(numChars[i])
+            }
+        }
+        else {
+            println("Invalid file format: Not enough lines in the file.")
+        }
     }
 }
